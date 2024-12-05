f075c7c2be407e85c7ff48e9cc386f4bac77c00c
Gustavo Sperandio <gusperandio2@gmail.com>
2024-12-05 16:42:05 -0300

    fix values info

--- src/components/FormHeader.tsx
+++ src/components/FormHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../style/formheader.css";
 
 const FormHeader: React.FC = () => {
   return (
@@ -6,6 +7,10 @@ const FormHeader: React.FC = () => {
       <h1 className="form-title">
         <b>RCKT CAMP 2025 | </b>Se prepare para o <b>AVIVAMENTO</b>
       </h1>
+      <p className="form-subtitle">Nosso acampamento acontecerá do dia 07 ao 09 de março.
+      </p>
+      <p className="form-subtitle"><b>Primeiro lote:</b> <span>R$</span> 340<span>,00</span>
+      </p>
       <p className="form-subtitle">
         Viveremos coisas incríveis e precisamos que você preencha todas as
         informações, assim poderemos te direcionar da melhor maneira.
