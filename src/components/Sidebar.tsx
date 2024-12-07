85f394b7a2bb040c0d79274eda7a4ed3e2d0cdb0
Gustavo Sperandio <gusperandio2@gmail.com>
2024-12-06 21:08:41 -0300

    ID-FIX

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import logo from "../logo-camp.svg"; // Substitua pelo caminho do logo
 import "../style/Sidebar.css";
-import logo from "./Layer_1.svg"; // Substitua pelo caminho do logo
 
 const Sidebar: React.FC = () => {
   return (
@@ -9,12 +9,8 @@ const Sidebar: React.FC = () => {
         <img src={logo} alt="Logo" />
       </div>
       <div className="sidebar-content">
-        <h1>Bem-Vindo!</h1>
+        <h1>BEM-VINDOS!</h1>
         <p>Faça agora sua inscrição para o RCKT CAMP 25</p>
-      </div>
-      <div className="sidebar-footer">
-        <h2>Avivamento</h2>
-        <p>Atos 2:17-21</p>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
-</style>
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
